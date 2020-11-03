782bfbcd618e87f6f9df95444678a13b997d461b
Lennox <omondilenox@outlook.com>
2020-11-03 06:49:43 +0300

    added crud for notifications and posts model

--- myHood_Main/models.py
+++ myHood_Main/models.py
@@ -46,9 +46,15 @@ class Post(models.Model):
     CREATED_AT = models.DateTimeField(auto_now_add=True)
     UPDATED_AT = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.hood.name} post #{self.id}"
+
 
 class Notification(models.Model):
-    content = models.TextField
+    content = models.TextField()
     hood = models.ForeignKey(Hood, on_delete=models.CASCADE, related_name='notifications')
     CREATED_AT = models.DateTimeField(auto_now_add=True)
     UPDATED_AT = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Notification {self.hood.name} #{self.id}"
