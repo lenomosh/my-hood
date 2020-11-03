089cb5799170100bf763733071e781c16dd1de59
Lennox <omondilenox@outlook.com>
2020-11-03 06:48:59 +0300

    added models to admin dashboard

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,9 +1,13 @@
+from django import forms
 from django_registration.forms import RegistrationForm
 
 # Create your views here.
+from myHood_Main.models import Hood
 from .models import User
 
 
 class CustomUserForm(RegistrationForm):
+    hood = forms.Select(choices=Hood.objects.all())
+
     class Meta(RegistrationForm.Meta):
         model = User
